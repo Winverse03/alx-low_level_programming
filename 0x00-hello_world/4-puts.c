2712bd47748d64834323f4561b8a5442d65f8f70
Winverse03 <nwills03@gmail.com>
2022-10-13 16:11:01 -0700

    4 puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /**
  * main - prints the phrase "Programming is like building a multilingual puzzle"
  *
- * Return: 0 if exited properly, non zero then do otherwise
+ * Return: 0 if exited properly, not zero do otherwise
  */
 int main(void)
 {
