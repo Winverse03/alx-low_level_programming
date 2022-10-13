bb31f6e27bf064feeac4167bc3d426da136ec0ce
Winverse03 <nwills03@gmail.com>
2022-10-13 16:52:33 -0700

    4 puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints the phrase "Programming is like building a multilingual puzzle"
+ * main - prints the phrase Programming is like building a multilingual puzzle
  * Return: 0 (success)
  */
 int main(void)
