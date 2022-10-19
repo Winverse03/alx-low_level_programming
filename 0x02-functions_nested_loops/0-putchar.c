a6bf258561d869b846c040d796ae1aa306d51343
Winverse03 <nwills03@gmail.com>
2022-10-18 22:12:11 -0700

    headers

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main -  prints Holberton, followed by a new line
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	_putchar('A');
+	_putchar('L');
+	_putchar('X');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
