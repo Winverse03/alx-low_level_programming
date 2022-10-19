d4602502e5eb64abb8a803064697384c8948040b
Winverse03 <nwills03@gmail.com>
2022-10-19 16:45:52 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,13 +7,12 @@
 
 void print_alphabet(void)
 {
-	char ch;
-	
-	ch = 'a';
-	while (ch <= 'z')
-	{
-		_putchar(ch);
-		ch++;
-	}
+	char ch
+		ch = 'a';
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
 	_putchar('\n');
 }
