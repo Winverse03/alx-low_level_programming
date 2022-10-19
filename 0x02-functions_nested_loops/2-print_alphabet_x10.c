33543c07910021aa679b15c4ad5fcd2d4483e7d6
Winverse03 <nwills03@gmail.com>
2022-10-19 16:33:09 -0700

    multiple alphabets x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,6 +9,7 @@ void print_alphabet_x10(void)
 {
 	char ch;
 	int i;
+
 	i = 0;
 	while (i < 10)
 	{
@@ -17,7 +18,7 @@ void print_alphabet_x10(void)
 		{
 			_putchar(ch);
 			ch++;
-		}	
+		}
 		_putchar('\n');
 		i++;
 	}
