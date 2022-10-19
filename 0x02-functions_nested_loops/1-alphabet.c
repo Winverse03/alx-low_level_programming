8092f6e5a102290ed1373f430beae66e0180b3ea
Winverse03 <nwills03@gmail.com>
2022-10-19 16:38:50 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - this prints lowercase alphabets
+ * followed by a new line
+ */
+
+void print_alphabet(void)
+{
+	char ch;
+	
+	ch = 'a';
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+}
