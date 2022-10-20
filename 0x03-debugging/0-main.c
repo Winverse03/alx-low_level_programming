5ecaffc408661d9db6c5e0407cd5c2db751cc75f
Winverse03 <nwills03@gmail.com>
2022-10-20 13:50:10 -0700

    0debug

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -5,8 +5,16 @@
  * Return: 0
  */
 
-void positive_or_negative(int i)
+#include "main.h"
+
+/**
+ * main - main tests positive or negative
+ * Return: 0
+ */
+int main(void)
 {
+	int i;
+
 	i = 0;
 	positive_or_negative(i);
 	return (0);
