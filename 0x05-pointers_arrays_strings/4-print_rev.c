b9916f9306530859bf308dbfe7c69f4b45839ee6
Winverse03 <nwills03@gmail.com>
2022-10-24 21:51:31 -0700

    reverse string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,6 +1,8 @@
 #include "main.h"
+
 /*
- * print_rev - prints a string in reverse, followed by a new line
+ * print_rev: prints a string in reverse, 
+ * followed by a new line
  * @s: string to be printed
  */
 
