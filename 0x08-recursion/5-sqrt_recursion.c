69dd46af0d48df8235a7a7657fd605d1a695d9f8
Winverse03 <nwills03@gmail.com>
2022-11-05 04:30:03 -0700

    a function that returns the natural square root of a number.

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,14 +2,31 @@
 
 /**
  * _sqrt_recursion - returns the natural square root of a number
+ * find_sqrt - Finds the natural square root of an inputted number.
+ * @num: The number to find the square root of
+ * @root: The root to be tested.
  * @n: integer to be square rooted
  * Return: the natural square root found
  */
 
+int find_sqrt(int num, int root)
+{
+
+	if ((root * root) == num)
+		return (root);
+	if (root == num / 2)
+		return (-1);
+	return (find_sqrt(num, root + 1));
+
+}
 int _sqrt_recursion(int n)
 {
 
-	int a = 1;
+	int root = 0;
 
-	return (sqrt_recursion(a, n));
+	if (n < 0)
+		return (-1);
+	if (n == 1)
+		return (1);
+	return (find_sqrt(n, root));
 }
