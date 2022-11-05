8a0d190e91a1fc31da8e098c300282478109b903
Winverse03 <nwills03@gmail.com>
2022-11-05 11:52:26 -0700

    a program that adds positive numbers.

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * main - Program that takes in all integer arguments and returns the sum
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 1 if a non-integer is among the passed in arguments, 0 otherwise
+ */
+
+int main(int argc, char *argv[])
+{
+
+	int a, b, length, sum;
+	char *pointer;
+
+	if (argc < 2)
+		printf("0\n");
+	else
+	{
+		sum = 0;
+		for (a = 1; a < argc; a++)
+		{
+			pointer = argv[a];
+			length = strlen(pointer);
+			for (b = 0; b < length; b++)
+			{
+				if (isdigit(*(pointer + b)) == 0)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			sum += atoi(argv[a]);
+		}
+		printf("%d\n", sum);
+	}
+	return (0);
+}
