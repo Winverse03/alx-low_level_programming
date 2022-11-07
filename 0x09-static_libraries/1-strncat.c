30eb063f3540ca663e1c2d8dac3254be2b44af06
Winverse03 <nwills03@gmail.com>
2022-11-06 18:18:51 -0800

    necessary files for static libraries

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -1,22 +1,27 @@
 #include "main.h"
 
 /**
- * _strncat - Concatenates two strings using at most
- * an inputted number of bytes from src.
- * @dest: The string to be appended upon.
- * @src: The string to be appended to dest.
- * @n: The number of bytes from src to be appended to dest.
- * Return: A pointer to the resulting string dest.
+ * _strncat - concatenates n bytes from a string to another
+ * @dest: destination string
+ * @src: source string
+ * @n: number of bytes of str to concatenate
+ * Return: a pointer to the resulting string dest
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
+	int i, j;
 
-	int i = 0, d = 0;
-
-	while (dest[i++])
-		d++;
-	for (i = 0; src[i] && i < n; i++)
-		dest[d++] = src[i];
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
 	return (dest);
 }
