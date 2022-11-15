39aa620f924eabb4e4cb94c5488a9ab0747602b2
Winverse03 <nwills03@gmail.com>
2022-11-14 18:23:14 -0800

    inputing a header

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code for ALX preprocessor task
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+
+	puts(__BASE_FILE__);
+	return (0);
+}
