cab221c1d3b11af009a35c678866afde3ff17b5c
Winverse03 <nwills03@gmail.com>
2022-11-15 19:12:11 -0800

    a function that initialize a variable of type struct dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -4,9 +4,9 @@
  * init_dog - Create a Dog.
  * @n: name of struct
  * @name: Name of Dog.
- * @age: Age of Dog.
- * @owner: owner of Dog.
- * 
+ * @age: Age of Dog
+ * @owner: owner of Dog
+ *
  * Return: Always 0.
  */
 void init_dog(struct dog *n, char *name, float age, char *owner)
