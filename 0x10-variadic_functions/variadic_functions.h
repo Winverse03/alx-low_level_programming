1a5c49246fa4c3e189ce6d1f0f8d54820f0daf5f
Winverse03 <nwills03@gmail.com>
2022-11-17 20:41:24 -0800

    a function that returns the sum of all its parameters.

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,25 @@
+#ifndef VAR_H
+#define VAR_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * struct select - create s.
+ * @q: inputa symbol representing a data type.
+ * @u: input  A function pointer to a function that prints
+ * a data type corresponding to symbol.
+ * Return: int */
+
+typedef struct select
+{
+
+	char *q;
+	void (*u)(va_list pa);
+} tstruc;
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+#endif
