e6652acd5267d7f6e4e5658599114ef7c909c1cc
Winverse03 <113660470+Winverse03@users.noreply.github.com>
2022-12-04 23:52:54 +0100

    add new node corrected

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,12 +1,13 @@
 include "lists.h"
 
 /**
- * add_nodeint - add a new node at the beginning
- * of a linked list
+ * add_nodeint - add a new node 
+ * at the beginning of a linked list
  * @head: head of a list.
  * @n: n element
  * Return: address of the new element. NUll if it failed.
  */
+
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new_node;
@@ -14,8 +15,10 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
+	
 	new_node->n = n;
 	new_node->next = *head;
 	*head = new_node;
+	
 	return (*head);
 }
