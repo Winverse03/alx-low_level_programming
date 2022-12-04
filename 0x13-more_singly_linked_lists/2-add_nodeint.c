458184733941db72fae8d0338e24fb42d73d3117
Winverse03 <113660470+Winverse03@users.noreply.github.com>
2022-12-04 23:56:36 +0100

    Update 2-add_nodeint.c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * add_nodeint - add a new node 
+ * add_nodeint - add a new node
  * at the beginning of a linked list
  * @head: head of a list.
  * @n: n element
@@ -15,10 +15,8 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
-	
 	new_node->n = n;
 	new_node->next = *head;
 	*head = new_node;
-	
 	return (*head);
 }
